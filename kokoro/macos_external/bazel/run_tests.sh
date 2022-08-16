88e6fa017badd44abe89bf5128cece95c8b7fd1b
Moreno Ambrosin <ambrosin@google.com>
2022-08-16 04:39:37 -0700

    Add test scripts for MacOS testing
    PiperOrigin-RevId: 467893262
    Change-Id: I117772f4ef810c38e913d19e68390385eab6a1b9

---
+++ kokoro/macos_external/bazel/run_tests.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+# The user may specify TINK_BASE_DIR as the folder where to look for
+# tink-go-awskms dependencies:
+#   - tink-go.
+
+set -euo pipefail
+
+if [[ -n "${KOKORO_ROOT:-}" ]]; then
+  cd "${KOKORO_ARTIFACTS_DIR}/git/tink_go_gcpkms"
+  use_bazel.sh "$(cat .bazelversion)"
+fi
+
+# When running on the Kokoro CI, we expect these folders to exist:
+#
+#  ${KOKORO_ARTIFACTS_DIR}/git/tink_go
+#  ${KOKORO_ARTIFACTS_DIR}/git/tink_go_gcpkms
+#
+# If this is not the case, we are using this script locally for a manual one-off
+# test (running it from the root of a local copy of the tink-go-awskms repo).
+: "${TINK_BASE_DIR:=$(pwd)/..}"
+
+# If tink-go is not in TINK_BASE_DIR we clone it from GitHub.
+if [[ ! -d "${TINK_BASE_DIR}/tink_go" ]]; then
+  git clone https://github.com/tink-crypto/tink-go.git \
+    "${TINK_BASE_DIR}/tink_go"
+fi
+
+# Sourcing required to update callers environment.
+source ./kokoro/testutils/install_go.sh
+
+echo "Using go binary from $(which go): $(go version)"
+
+# TODO(b/238389921): Run check_go_generated_files_up_to_date.sh after a
+# refactoring that takes into account extensions to tink-go.
+
+./kokoro/testutils/copy_credentials.sh "testdata" "gcp"
+
+cp WORKSPACE WORKSPACE.bak
+
+# Replace com_github_tink_crypto_tink_go with a local one.
+grep -r "com_github_tink_crypto_tink_go" -l --include="*.bazel" \
+  | xargs sed -i \
+      "s~com_github_tink_crypto_tink_go~com_github_tink_crypto_tink_go_local~g"
+
+sed -i 's~workspace(name = "tink_go_gcpkms")~workspace(name = "tink_go_gcpkms")\
+\
+local_repository(\
+    name = "com_github_tink_crypto_tink_go_local",\
+    path = "'"${TINK_BASE_DIR}"'/tink_go",\
+)~' WORKSPACE
+
+MANUAL_TARGETS=()
+# Run manual tests that rely on test data only available via Bazel.
+if [[ -n "${KOKORO_ROOT:-}" ]]; then
+  MANUAL_TARGETS+=( "//integration/gcpkms:gcpkms_test" )
+fi
+readonly MANUAL_TARGETS
+
+./kokoro/testutils/run_bazel_tests.sh . "${MANUAL_TARGETS[@]}"
+
+mv WORKSPACE.bak WORKSPACE
---
+++ kokoro/macos_external/gomod/run_tests.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+# The user may specify TINK_BASE_DIR as the folder where to look for
+# tink-go-gcpkms dependencies.
+
+set -euo pipefail
+
+TINK_GO_GCPKMS_PROJECT_PATH="$(pwd)"
+if [[ -n "${KOKORO_ROOT:-}" ]]; then
+  TINK_GO_GCPKMS_PROJECT_PATH="${KOKORO_ARTIFACTS_DIR}/git/tink_go_gcpkms"
+  cd "${TINK_GO_GCPKMS_PROJECT_PATH}"
+fi
+readonly TINK_GO_GCPKMS_PROJECT_PATH
+
+# When running on the Kokoro CI, we expect these folders to exist:
+#
+#  ${KOKORO_ARTIFACTS_DIR}/git/tink_go
+#  ${KOKORO_ARTIFACTS_DIR}/git/tink_go_gcpkms
+#
+# If this is not the case, we are using this script locally for a manual one-off
+# test (running it from the root of a local copy of the tink-go-gcpkms repo).
+: "${TINK_BASE_DIR:=$(pwd)/..}"
+
+# If tink-go is not in TINK_BASE_DIR we clone it from GitHub.
+if [[ ! -d "${TINK_BASE_DIR}/tink_go" ]]; then
+  git clone https://github.com/tink-crypto/tink-go.git \
+    "${TINK_BASE_DIR}/tink_go"
+fi
+
+./kokoro/testutils/copy_credentials.sh "testdata" "gcp"
+# Sourcing required to update callers environment.
+source ./kokoro/testutils/install_go.sh
+
+echo "Using go binary from $(which go): $(go version)"
+
+readonly TINK_GO_MODULE_URL="github.com/tink-crypto/tink-go"
+readonly TINK_GO_GCPKMS_MODULE_URL="github.com/tink-crypto/tink-go-gcpkms"
+readonly TINK_VERSION="$(cat ${TINK_GO_GCPKMS_PROJECT_PATH}/tink_version.bzl \
+                        | grep ^TINK \
+                        | cut -f 2 -d \")"
+
+
+cp go.mod go.mod.bak
+
+# Modify go.mod locally to use the version of tink-go in TINK_BASE_DIR/tink_go.
+go mod edit "-replace=${TINK_GO_MODULE_URL}=${TINK_BASE_DIR}/tink_go"
+go list -m all | grep tink-go
+
+./kokoro/testutils/run_go_mod_tests.sh \
+  "${TINK_GO_GCPKMS_MODULE_URL}" \
+  "${TINK_GO_GCPKMS_PROJECT_PATH}" \
+  "${TINK_VERSION}" \
+  "main"
+
+mv go.mod.bak go.mod
