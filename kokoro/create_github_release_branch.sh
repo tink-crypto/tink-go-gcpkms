efe7dba46d994db6a5d1ce33efffcb3b4583223a
Moreno Ambrosin <ambrosin@google.com>
2023-06-22 01:35:35 -0700

    Use the correct variable name in kokoro/create_github_release_branch.sh
    PiperOrigin-RevId: 542488584
    Change-Id: I97bf66567f67744fc564bb5499642d280b8081b0

--- kokoro/create_github_release_branch.sh
+++ kokoro/create_github_release_branch.sh
@@ -47,9 +47,9 @@ fi
 
 GITHUB_RELEASE_UTIL_OPTS=()
 if [[ "${IS_KOKORO}" == "true" ]] ; then
-  # Note: KOKORO_GIT_COMMIT is populated by Kokoro.
+  # Note: KOKORO_GIT_COMMIT_tink_go_gcpkms is populated by Kokoro.
   GITHUB_RELEASE_UTIL_OPTS+=(
-    -c "${KOKORO_GIT_COMMIT}"
+    -c "${KOKORO_GIT_COMMIT_tink_go_gcpkms}"
     -t "${GITHUB_ACCESS_TOKEN}"
   )
 fi
